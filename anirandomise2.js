98caf1d2d8cac0f320606948d0a3b3d4ff046fbe
risonakamo <hmykanae@gmail.com>
2019-10-20 20:32:41 -0400

    vid set creation

--- anirandomise2.js
+++ anirandomise2.js
@@ -1,10 +1,17 @@
 const _=require("lodash");
 const yaml=require("js-yaml");
 const fs=require("fs-extra");
+const path=require("path");
 
 function main()
 {
-    var config=yaml.safeLoad(fs.readFileSync("test.yml"));
+    if (!fs.existsSync("config.yml"))
+    {
+        console.log("missing config.yml");
+        return;
+    }
+
+    var config=yaml.safeLoad(fs.readFileSync("config.yml"));
 
     var requiredOptions={
         "itemspath":"absolute path to folder where randomise will pick from",
@@ -32,7 +39,56 @@ function main()
         return;
     }
 
-    console.log(checkPaths(config));
+    checkPaths(config);
+
+    var dirItems=fs.readdirSync(config.itemspath,{withFileTypes:true});
+    dirItems=_.filter(dirItems,(x)=>{
+        return x.isFile();
+    });
+
+    var vidset=convertVidSet(dirItems);
+
+    console.log(vidset);
+}
+
+//given a readdir array, convert it into a vidset, a dict:
+/*{
+    [simplifiedName]: [string]  //array of file names corresponding to the
+                                //simplified name key
+  }*/
+function convertVidSet(files)
+{
+    var smallname;
+    return _.reduce(files,(r,x)=>{
+        smallname=simplifyName(x.name);
+
+        if (!smallname.length)
+        {
+            return r;
+        }
+
+        if (!r[smallname])
+        {
+            r[smallname]=[];
+        }
+
+        r[smallname].push(x.name);
+
+        return r;
+    },{});
+}
+
+//given a string name, return the simplified version of it, or nothing if it
+//is not the correct file type
+function simplifyName(name)
+{
+    var extension=path.extname(name);
+    if (!(extension==".mkv" || extension==".mp4"))
+    {
+        return "";
+    }
+
+    return name.replace(/\[.*?\]|\.mkv|\.mp4/g,"").replace(/[^\w]|\d/g,"").toLowerCase();
 }
 
 //given the config, makes sure the paths exist by creating them
