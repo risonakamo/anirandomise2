624de461ae9c67bc7866634e73a273b2cf8f1a16
risonakamo <hmykanae@gmail.com>
2020-03-08 14:21:54 -0400

    possibly working stats module

---
+++ anirandomise2-stats.js
@@ -0,0 +1,35 @@
+const yaml=require("js-yaml");
+const fs=require("fs-extra");
+
+function recordStats(statsFilePath,choice,numberChoices)
+{
+    var statsfile;
+    if (fs.existsSync(statsFilePath))
+    {
+        statsfile=yaml.safeLoad(fs.readFileSync(statsFilePath));
+    }
+
+    else
+    {
+        statsfile={};
+    }
+
+    if (!statsfile[choice])
+    {
+        statsfile[choice]=[];
+    }
+
+    statsfile[choice].push(numberChoices);
+
+    fs.outputFile(statsFilePath,yaml.safeDump(statsfile,{flowLevel:1}));
+}
+
+function main()
+{
+    recordStats("stats.yml","a",4);
+}
+
+if (!module.parent)
+{
+    main();
+}
