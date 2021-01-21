87de90571e9ae9646c459873404b29b365be9f0d
risonakamo <hmykanae@gmail.com>
2021-01-21 01:13:01 -0500

    calculate pick duplicate percent

--- anirandomise.ts
+++ anirandomise.ts
@@ -1,6 +1,6 @@
 import {retrieveShows} from "./lib/show-resolvers";
 import {addShort,dataServiceTest} from "./lib/data-service";
-import {cspSample,testRandom} from "./lib/randomisation";
+import {cspSample,testRandom,testRandom2} from "./lib/randomisation";
 
 const _vidsPath:string="C:\\Users\\ktkm\\Desktop\\anirandomise3\\testzone\\vids";
 const _deletePath:string="C:\\Users\\ktkm\\Desktop\\anirandomise3\\testzone\\delete";
@@ -12,7 +12,8 @@ async function main()
 {
     // var shows:ShowsDict=retrieveShows(_vidsPath);
     // testRandom(Object.values(shows),10000);
-    dataServiceTest();
+    // dataServiceTest();
+    testRandom2();
 }
 
 main();
--- lib/randomisation.ts
+++ lib/randomisation.ts
@@ -12,6 +12,20 @@ export async function cspSample<T>(array:T[]):Promise<T>
     return array[await randomNumber(0,array.length-1)];
 }
 
+/** given a number of items, return if duplicate should be chosen using formula */
+async function duplicateChance(items:number,reductionChance:number=.5,maxChance:number=.25):Promise<boolean>
+{
+    if (items==1)
+    {
+        return true;
+    }
+
+    var successChance:number=Math.min((1/items)*reductionChance,maxChance);
+    var randomVal:number=(await randomNumber(0,1000))/1000;
+
+    return randomVal<successChance;
+}
+
 /** test cspSample function. give it array of shows to pick from and number of times to perform picks. */
 export async function testRandom(shows:Show[],numPicks:number):Promise<void>
 {
@@ -32,4 +46,9 @@ export async function testRandom(shows:Show[],numPicks:number):Promise<void>
 
     console.log(percentages);
     console.log("expected average:",(1/shows.length)*100);
+}
+
+export async function testRandom2():Promise<void>
+{
+    console.log(await duplicateChance(4));
 }
