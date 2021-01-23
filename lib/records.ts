f1cbd68826348d5dcd5c0cc4b5bc0ad27fc7fdb2
risonakamo <hmykanae@gmail.com>
2021-01-23 14:54:03 -0500

    setting previous selection in data

--- lib/records.ts
+++ lib/records.ts
@@ -1,13 +1,16 @@
 import prependFile from "prepend-file";
 import {statSync} from "fs";
 
+import {setPreviousSelection} from "../lib/data-service";
+
 /** record a show to the target log file */
-export function recordShow(show:Show,logFile:string)
+export function recordShow(show:Show,logFile:string):Promise<void>
 {
     if (!statSync(logFile).isFile())
     {
         throw `log file ${logFile} was not a file`;
     }
 
-    prependFile(logFile,`${show.topShow}\n`);
+    setPreviousSelection(show.shortname);
+    return prependFile(logFile,`${show.topShow.filename}\n`);
 }
--- tests/records.spec.ts
+++ tests/records.spec.ts
@@ -10,7 +10,7 @@ describe.only("record show tests",()=>{
     const logFile:string="C:\\Users\\ktkm\\Desktop\\anirandomise3\\testzone\\randomise.log";
     const badLogFile:string="C:\\Users\\ktkm\\Desktop\\anirandomise3\\testzone";
 
-    it("should record to the log file",async ()=>{
+    it("should record to the log file and set the previous selected field in data.json",async ()=>{
         var shows:ShowsDict=retrieveShows(vidsPath);
         var picked:Show=await pickShow(shows);
 
@@ -19,12 +19,43 @@ describe.only("record show tests",()=>{
         recordShow(picked,logFile);
     });
 
-    it("should fail because the log file is a folder",async ()=>{
+    it.only(`should fail because the log file is a folder
+    and should NOT set the previous selected field`,async ()=>{
         var shows:ShowsDict=retrieveShows(vidsPath);
         var picked:Show=await pickShow(shows);
 
+        console.log("picked",picked);
+
         assert.throws(()=>{
-            recordShow(picked,badLogFile);
+            try
+            {
+                recordShow(picked,badLogFile);
+            }
+
+            catch (err)
+            {
+                throw err;
+            }
+        });
+    });
+
+    it.only(`same as should fail test, but should NOT fail because this time
+    a correct log file is given`,async ()=>{
+        var shows:ShowsDict=retrieveShows(vidsPath);
+        var picked:Show=await pickShow(shows);
+
+        console.log("picked",picked);
+
+        assert.doesNotThrow(()=>{
+            try
+            {
+                recordShow(picked,logFile);
+            }
+
+            catch (err)
+            {
+                throw err;
+            }
         });
     });
 });
