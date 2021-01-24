08fe66fb69d6a64859d46dd85c799759ed5cfec3
risonakamo <hmykanae@gmail.com>
2021-01-23 20:33:12 -0500

    records properly creating log logfile timestamp

--- lib/records.ts
+++ lib/records.ts
@@ -1,16 +1,44 @@
 import prependFile from "prepend-file";
-import {statSync} from "fs";
+import {statSync,Stats} from "fs";
+import {DateTime} from "luxon";
+import ck from "chalk";
 
 import {setPreviousSelection} from "../lib/data-service";
 
 /** record a show to the target log file */
 export function recordShow(show:Show,logFile:string):Promise<void>
 {
-    if (!statSync(logFile).isFile())
+    try
     {
-        throw `log file ${logFile} was not a file`;
+        var logfileStat:Stats=statSync(logFile);
+
+        // if it exists and is a directory, problem
+        if (logfileStat && !logfileStat.isFile())
+        {
+            throw `log file ${logFile} was not a file`;
+        }
+    }
+
+    catch (err)
+    {
+        // if it doesnt exist, dont care, it will be created.
+        if (err.code=="ENOENT")
+        {
+            console.log(ck.blue(`creating logfile at ${ck.yellow(logFile)}`));
+        }
+
+        else
+        {
+            throw err;
+        }
     }
 
     setPreviousSelection(show.shortname);
-    return prependFile(logFile,`${show.topShow.filename}\n`);
+    return prependFile(logFile,`${customTimeStamp()} ${show.topShow.filename}\n`);
+}
+
+/** get timestamp string for current time*/
+function customTimeStamp():string
+{
+    return DateTime.local().toFormat("yyyy-LL-dd HH:mm:ss");
 }
--- tests/records.spec.ts
+++ tests/records.spec.ts
@@ -5,12 +5,13 @@ import {pickShow} from "../lib/randomisation";
 
 import {recordShow} from "../lib/records";
 
-describe("record show tests",()=>{
+describe.only("record show tests",()=>{
     const vidsPath:string="C:\\Users\\ktkm\\Desktop\\anirandomise3\\testzone\\vids";
     const logFile:string="C:\\Users\\ktkm\\Desktop\\anirandomise3\\testzone\\randomise.log";
     const badLogFile:string="C:\\Users\\ktkm\\Desktop\\anirandomise3\\testzone";
 
-    it("should record to the log file and set the previous selected field in data.json",async ()=>{
+    it(`should record to the log file and set the previous selected field in data.json. should create log file if did
+    not exist before.`,async ()=>{
         var shows:ShowsDict=retrieveShows(vidsPath);
         var picked:Show=await pickShow(shows);
 
