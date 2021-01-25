62af5ce53327754ce776b76123458ed8cc43f651
risonakamo <hmykanae@gmail.com>
2021-01-25 00:26:52 -0500

    move output tests together

--- tests/output.spec.ts
+++ tests/output.spec.ts
@@ -6,55 +6,55 @@ import {pickShow} from "../lib/randomisation";
 import {outputTests} from "../lib/output";
 const {printChoice,printShowListStats,calcUniqueCounts,calcItemCounts}=outputTests;
 
-describe("print choice tests",async ()=>{
+describe.only("output tests",()=>{
     const vidsPath="C:\\Users\\ktkm\\Desktop\\anirandomise3\\testzone\\vids";
 
-    it("should print out a choice",async ()=>{
-        await printChoiceTest(vidsPath);
+    describe("print choice tests",async ()=>{
+        it("should print out a choice",async ()=>{
+            await printChoiceTest(vidsPath);
+        });
+
+        it("should print out a choice 2",async ()=>{
+            await printChoiceTest(vidsPath);
+        });
+
+        it("should print out a choice 3",async ()=>{
+            await printChoiceTest(vidsPath);
+        });
+
+        it("should print out a choice 4",async ()=>{
+            await printChoiceTest(vidsPath);
+        });
+
+        it("should print out show list counts",async ()=>{
+            var shows:ShowsDict=retrieveShows(vidsPath);
+            var picked:Show=await pickShow(shows);
+            console.log("picked",picked.shortname);
+            printShowListStats(shows,picked);
+        });
     });
 
-    it("should print out a choice 2",async ()=>{
-        await printChoiceTest(vidsPath);
-    });
+    describe("print show stats",()=>{
+        it("should print out show list counts",async ()=>{
+            var shows:ShowsDict=retrieveShows(vidsPath);
+            var picked:Show=await pickShow(shows);
+            console.log("picked",picked.shortname);
+            printShowListStats(shows,picked);
+        });
 
-    it("should print out a choice 3",async ()=>{
-        await printChoiceTest(vidsPath);
-    });
+        it("should print out unique counts object",()=>{
+            var shows:ShowsDict=retrieveShows(vidsPath);
+            var res:ShowCounts=calcUniqueCounts(shows);
+            console.log("unique counts",res);
+            assert.isObject(res);
+        });
 
-    it("should print out a choice 4",async ()=>{
-        await printChoiceTest(vidsPath);
-    });
-
-    it("should print out show list counts",async ()=>{
-        var shows:ShowsDict=retrieveShows(vidsPath);
-        var picked:Show=await pickShow(shows);
-        console.log("picked",picked.shortname);
-        printShowListStats(shows,picked);
-    });
-});
-
-describe.only("print show stats",()=>{
-    const vidsPath="C:\\Users\\ktkm\\Desktop\\anirandomise3\\testzone\\vids";
-
-    it.only("should print out show list counts",async ()=>{
-        var shows:ShowsDict=retrieveShows(vidsPath);
-        var picked:Show=await pickShow(shows);
-        console.log("picked",picked.shortname);
-        printShowListStats(shows,picked);
-    });
-
-    it("should print out unique counts object",()=>{
-        var shows:ShowsDict=retrieveShows(vidsPath);
-        var res:ShowCounts=calcUniqueCounts(shows);
-        console.log("unique counts",res);
-        assert.isObject(res);
-    });
-
-    it("should print out item counts object",()=>{
-        var shows:ShowsDict=retrieveShows(vidsPath);
-        var res:ShowCounts=calcItemCounts(shows);
-        console.log("item counts",res);
-        assert.isObject(res);
+        it("should print out item counts object",()=>{
+            var shows:ShowsDict=retrieveShows(vidsPath);
+            var res:ShowCounts=calcItemCounts(shows);
+            console.log("item counts",res);
+            assert.isObject(res);
+        });
     });
 });
 
